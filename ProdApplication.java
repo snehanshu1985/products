b552234954154df45e783cdd12fd294091eef39f
snehanshu1985 <46123677+snehanshu1985@users.noreply.github.com>
2018-12-25 01:09:10 -0600

    Add files via upload

---
+++ ProdApplication.java
@@ -0,0 +1,14 @@
+package com.selfserve.arya.prod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdApplication.class, args);
+	}
+
+}
+
